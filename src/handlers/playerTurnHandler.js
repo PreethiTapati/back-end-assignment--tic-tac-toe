ad4435cb614e6d0769462c3d90d9e03dfdcf8ee3
PreethiTapati <preetimadhuri22@gmail.com>
2023-09-23 18:34:16 +0200

    fixed player handler

--- src/handlers/playerTurnHandler.js
+++ src/handlers/playerTurnHandler.js
@@ -3,39 +3,39 @@ import data from '../data.js';
 import selectWinner from '../utils/selectWinner.js';
 
 const playerTurnHandler = (e) => {
-    const cellEl = e.target;
-    const id = cellEl.id;
+  const cellEl = e.target;
+  const id = cellEl.id;
 
-    // Check if the cell is already marked or if the game has ended
-    if (cellEl.innerText || dom.turnInfo.innerHTML === `${data.turn} won!`) {
-        return;
-    }
+  // Check if cell is already marked or game is over
+  if (cellEl.innerText || dom.playerTurn.innerHTML === `${data.turn} won!`) {
+    return;
+  }
 
-    cellEl.classList.add(data.turn);
-    cellEl.innerHTML = data.turn;
+  // Mark the cell with the current player's mark
+  cellEl.classList.add(data.turn);
+  cellEl.innerHTML = data.turn;
 
-    // Update the game board with the player's move
-    data.playBoard[id] = data.turn;
+  // Update internal game board data
+  data.playBoard[id] = data.turn;
 
-    // Check for a winner using the checkWinner utility function
-    const winResult = selectWinner(
-        document.querySelectorAll(`.${data.turn}`),
-        data.winCombinations,
-        data.turn,
-        data.playBoard,
-    );
+  // Check for winner or draw
+  const winResult = selectWinner(
+    document.querySelectorAll(`.${data.turn}`),
+    data.winCombinations,
+    data.turn,
+    data.playBoard
+  );
 
-    // If there's a winner or it's a draw, update the turn information
-    if (winResult !== null) {
-        dom.turnInfo.innerHTML = winResult;
-        return;
-    }
+  if (winResult !== null) {
+    dom.playerTurn.innerHTML = winResult;
+    return;
+  }
 
-    // Switch the turn to the other player (X to O or vice versa)
-    data.turn === 'X' ? (data.turn = 'O') : (data.turn = 'X');
+  // Toggle current player
+  data.turn = (data.turn === 'O') ? 'X' : 'O';
 
-    // Update the turn information
-    dom.turnInfo.innerHTML = data.turn + "'s turn";
+  // Update UI with new player's turn
+  dom.playerTurn.innerHTML = `${data.turn}'s turn`;
 };
 
 export default playerTurnHandler;
